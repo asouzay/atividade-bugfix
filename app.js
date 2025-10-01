546dd725dac098c70e3a8a5d0ebb63c1b4a505ee
asouzay <maryani.silva@estudante.ifms.edu.br>
2025-10-01 16:35:39 -0400

    bug

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+
+// BUG 1: A variável está errada (deveria ser 'usuarioLogado')
+const user = "Alice"; 
+
+// BUG 2: A condição está invertida (deveria ser >= 18)
+const idade = 17;
+if (idade < 18) { 
+    console.log("Menor de idade, acesso negado.");
+}
+
+// BUG 3: Falta um ponto e vírgula e a função está errada (deveria ser console.log)
+function exibirMensagem() {
+    print("Bem-vindo" + user) 
+}
+
+exibirMensagem();
